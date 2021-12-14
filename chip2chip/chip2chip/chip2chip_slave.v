c09a3b92e8efc831d7fdaffeb883427ee93b69e4
Poyu Juan <yoyohihirobert@gmail.com>
2021-12-15 00:58:13 +0800

    correct chip2chip version

--- chip2chip/chip2chip/chip2chip_slave.v
+++ chip2chip/chip2chip/chip2chip_slave.v
@@ -61,25 +61,33 @@ module slave_control(
             state_wait_rqst: begin
                 next_state = (request == 1) ?
                 state_wait_to_send_ack : state_wait_rqst;
-                next_notice = 1'b0;
+                next_notice = (request == 1) ? 1'b1 : 1'b0;
                 next_ack = 1'b0;
-                next_data = 3'b000;
-                next_start = (request == 1) ? 1'b1:1'b0;
+                next_data = data;
+                next_start = (request == 1) ? 1'b1 : 1'b0;
             end
             state_wait_to_send_ack: begin
                 next_state = (done == 1) ?
-                state_wait_data : state_wait_to_send_ack;
-                next_notice = (done == 1) ? 1'b0 : 1'b1;
+                state_send_ack : state_wait_to_send_ack;
+                next_notice = 1'b1;
                 next_ack = (done == 1) ? 1'b1 : 1'b0;
-                next_data = 3'b000;
+                next_data = data;
                 next_start = (done == 1) ? 1'b0 : 1'b1;
+            end
+            state_send_ack: begin
+                next_state = state_wait_data;
+                next_notice = 1'b0;
+                next_ack = 1'b1;
+                next_data = data;
+                next_start = 1'b0;
             end
             state_wait_data: begin
                 next_state = (valid == 1) ?
-                state_wait_data : state_wait_rqst;
+                state_wait_rqst : state_wait_data;
                 next_notice = 1'b0;
                 next_ack = 1'b1;
-                next_data = (valid == 1) ? data_in : 3'b000;
+                next_data = (valid == 1) ? data_in : data;
+                //next_data = data_in;
                 next_start = 1'b0;
             end
             default: begin
